9946b039c3a438a6508fa47b904e68fb830d0f46
Dorian <dorian_hoet@hotmail.com>
2021-01-23 17:26:48 +0100

    Limitations layout

--- app/src/main/java/com/example/emptyproject/DrawingCanvaLayout.java
+++ app/src/main/java/com/example/emptyproject/DrawingCanvaLayout.java
@@ -1,6 +1,7 @@
 package com.example.emptyproject;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -8,8 +9,8 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
 import android.graphics.Rect;
-import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -19,24 +20,69 @@ import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.view.MotionEventCompat;
-import androidx.core.view.ScaleGestureDetectorCompat;
-import androidx.core.view.ViewCompat;
 
 import java.util.ArrayList;
 
 import static android.view.MotionEvent.INVALID_POINTER_ID;
 
+/**
+ * A drawable layout with zoom and scroll features
+ */
 public class DrawingCanvaLayout extends FrameLayout {
 
+    private final String TAG = "DrawingCanvaLayout";
+
+    private final float MAX_SCALE_FACTOR = 10f;
+    private final float MIN_SCALE_FACTOR = 0.1f;
+
+    private Rect layoutLimitsRectangle = new Rect(-100, -100, 3000, 3000);
+    DisplayMetrics displayMetrics = new DisplayMetrics();
+
+
     private Path path = new Path();
-    private final ArrayList<ColoredPath> pathList = new ArrayList();
+    private final ArrayList<ColoredPath> pathList = new ArrayList<>();
     private Paint brush = new Paint();
 
     private ScaleGestureDetector scaleGestureDetector;
 
 
+    private final GestureDetector.SimpleOnGestureListener gestureListener
+            = new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                float distanceX, float distanceY) {
+
+            translate(distanceX, distanceY);
+            invalidate();
+            return true;
+        }
+    };
+    private void translate(float distanceX, float distanceY) {
+        float newTranslateX = translateX - distanceX / scaleFactor;
+        float newTranslateY = translateY - distanceY / scaleFactor;
+
+        ((Activity) getContext()).getWindowManager()
+                .getDefaultDisplay()
+                .getMetrics(displayMetrics);
+
+        int screenHeight = displayMetrics.heightPixels;
+        int screenWidth = displayMetrics.widthPixels;
+
+        Log.d(TAG, "translate: " + -newTranslateX +" + "+ screenWidth +" < "+ layoutLimitsRectangle.right);
+        if (-newTranslateX + screenWidth/scaleFactor < layoutLimitsRectangle.right
+                && -newTranslateX > layoutLimitsRectangle.left)
+            translateX = newTranslateX;
+        if (-newTranslateY + screenHeight/scaleFactor < layoutLimitsRectangle.bottom
+                && -newTranslateY > layoutLimitsRectangle.top)
+            translateY = newTranslateY;
+
+    }
+
+    private GestureDetector gestureDetector;
+
+
     private float scaleFactor = 1.f;
-    private int mActivePointerId;
+    private int activePointerId;
     private float translateX = 0;
     private float translateY = 0;
 
@@ -67,6 +113,7 @@ public class DrawingCanvaLayout extends FrameLayout {
 
     private void init(Context context) {
         scaleGestureDetector = new ScaleGestureDetector(context, new ScaleListener());
+        gestureDetector = new GestureDetector(context, gestureListener);
     }
 
     private void setupBrush() {
@@ -81,14 +128,12 @@ public class DrawingCanvaLayout extends FrameLayout {
         this.brush = brush;
     }
 
-    float lastTouchX = 0;
-    float lastTouchY = 0;
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
         // Get the pointer ID
-        mActivePointerId = event.getPointerId(0);
+        activePointerId = event.getPointerId(0);
 
         // ... Many touch events later...
 
@@ -96,121 +141,67 @@ public class DrawingCanvaLayout extends FrameLayout {
 
         // Use the pointer ID to find the index of the active pointer
         // and fetch its position
-        int pointerIndex = event.findPointerIndex(mActivePointerId);
+        int pointerIndex = event.findPointerIndex(activePointerId);
 
 
         int action = MotionEventCompat.getActionMasked(event);
         // Get the index of the pointer associated with the action.
         int index = MotionEventCompat.getActionIndex(event);
-        int contactX = -1;
-        int contactY = -1;
+        int onScreenX = -1;
+        int onScreenY = -1;
+        /*
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_UP:
+                Log.d("MultyTouch", "onTouchEvent"+event.getPointerCount()+": UP");
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                Log.d("MultyTouch", "onTouchEvent"+event.getPointerCount()+": POINTER UP");
+                break;
+            case MotionEvent.ACTION_DOWN:
+                Log.d("MultyTouch", "onTouchEvent"+event.getPointerCount()+": DOWN");
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                Log.d("MultyTouch", "onTouchEvent"+event.getPointerCount()+": POINTER DOWN");
+                break;
+            case MotionEvent.ACTION_OUTSIDE:
+                Log.d("MultyTouch", "onTouchEvent"+event.getPointerCount()+": OUTSIDE");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.d("MultyTouch", "onTouchEvent"+event.getPointerCount()+": CANCEL");
+                break;
+        }
 
-
-        Log.d("Debug motion","The action is " + actionToString(action));
+         */
 
         if (event.getPointerCount() > 1) {
-            Log.d("Debug motion","Multitouch event");
-            // The coordinates of the current screen contact, relative to
-            // the responding View or Activity.
-            contactX = (int)MotionEventCompat.getX(event, index);
-            contactY = (int)MotionEventCompat.getY(event, index);
-            switch (action) {
-                case MotionEvent.ACTION_DOWN: {
-                    pointerIndex = MotionEventCompat.getActionIndex(event);
-                    final float touch_x = MotionEventCompat.getX(event, pointerIndex);
-                    final float touch_y = MotionEventCompat.getY(event, pointerIndex);
+            gestureDetector.onTouchEvent(event);
 
-                    // Remember where we started (for dragging)
-                    lastTouchX = touch_x;
-                    lastTouchY = touch_y;
-                    // Save the ID of this pointer (for dragging)
-                    mActivePointerId = MotionEventCompat.getPointerId(event, 0);
-                    break;
-                }
-
-                case MotionEvent.ACTION_MOVE: {
-                    // Find the index of the active pointer and fetch its position
-                    pointerIndex =
-                            MotionEventCompat.findPointerIndex(event, mActivePointerId);
-
-                    final float x = MotionEventCompat.getX(event, pointerIndex);
-                    final float y = MotionEventCompat.getY(event, pointerIndex);
-
-                    // Calculate the distance moved
-                    float deltaX = x - lastTouchX;
-                    float deltaY = y - lastTouchY;
-
-                    float newDeltaX = deltaX;
-                    float newDeltaY = deltaY;
-
-                    translateX += deltaX;
-                    translateY += deltaY;
-
-                    invalidate();
-
-                    // Remember this touch position for the next move event
-                    lastTouchX = x;
-                    lastTouchY = y;
-
-
-                    //Log.d("Debug motion scroll", String.valueOf(x));
-                    Log.d("Debug motion scroll", String.valueOf(deltaX));
-
-                    break;
-                }
-
-                case MotionEvent.ACTION_UP: {
-                    mActivePointerId = INVALID_POINTER_ID;
-                    break;
-                }
-
-                case MotionEvent.ACTION_CANCEL: {
-                    mActivePointerId = INVALID_POINTER_ID;
-                    break;
-                }
-
-                case MotionEvent.ACTION_POINTER_UP: {
-
-                    pointerIndex = MotionEventCompat.getActionIndex(event);
-                    final int pointerId = MotionEventCompat.getPointerId(event, pointerIndex);
-
-                    if (pointerId == mActivePointerId) {
-                        // This was our active pointer going up. Choose a new
-                        // active pointer and adjust accordingly.
-                        final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
-                        lastTouchX = MotionEventCompat.getX(event, newPointerIndex);
-                        lastTouchY = MotionEventCompat.getY(event, newPointerIndex);
-                        mActivePointerId = MotionEventCompat.getPointerId(event, newPointerIndex);
-                    }
-                    break;
-                }
-            }
 
             // TODO: end the previouly drown line or better event handling in single touch event
-            path.moveTo(contactX/ scaleFactor, contactY/ scaleFactor); //moyen...
+            path.moveTo(onScreenX/ scaleFactor, onScreenY/ scaleFactor); //moyen...
             // essayer de jouer avec le nomero de pointeur
+
 
             return true;
 
         } else {
             // Single touch event
-            Log.d("Debug motion","Single touch event");
-            contactX = (int)MotionEventCompat.getX(event, index);
-            contactY = (int)MotionEventCompat.getY(event, index);
+            onScreenX = (int)MotionEventCompat.getX(event, index);
+            onScreenY = (int)MotionEventCompat.getY(event, index);
 
             // detect drawing
             // float pointX = event.getX()/ scaleFactor;
             // float pointY = event.getY()/ scaleFactor;
 
-            float pointX = MotionEventCompat.getX(event, index)/ scaleFactor;
-            float pointY = MotionEventCompat.getY(event, index)/ scaleFactor;
+            float onLayoutX = onScreenX/ scaleFactor - translateX;
+            float onLayoutY = onScreenY/ scaleFactor - translateY;
 
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
-                    path.moveTo(pointX, pointY);
+                    path.moveTo(onLayoutX, onLayoutY);
                     return true;
                 case MotionEvent.ACTION_MOVE:
-                    path.lineTo(pointX, pointY);
+                    path.lineTo(onLayoutX, onLayoutY);
                     break;
                 case MotionEvent.ACTION_UP:
                     pathList.add(new ColoredPath(path, brush));
@@ -250,6 +241,11 @@ public class DrawingCanvaLayout extends FrameLayout {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        //((Activity) getContext()).getWindowManager()
+        //        .getDefaultDisplay()
+        //        .getMetrics(displayMetrics);
+        //Log.d("height", "onDraw: "+ getHeight()*scaleFactor + " x " + getWidth()*scaleFactor);
+        //Log.d("height", "onDraw: "+ displayMetrics.heightPixels + " x " + displayMetrics.widthPixels);
 
         canvas.save();
         canvas.scale(scaleFactor, scaleFactor);
@@ -270,34 +266,32 @@ public class DrawingCanvaLayout extends FrameLayout {
 
 
 
-        private class ScaleListener
-            extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+    private class ScaleListener
+        extends ScaleGestureDetector.SimpleOnScaleGestureListener {
         /**
          * This is the active focal point in terms of the viewport. Could be a local
          * variable but kept here to minimize per-frame allocations.
          */
-        private PointF viewportFocus = new PointF();
-        @Override
-        public boolean onScaleBegin(ScaleGestureDetector detector) {
-
-            return true;
-        }
-
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
             scaleFactor *= detector.getScaleFactor();
 
             // Don't let the object get too small or too large.
-            scaleFactor = Math.max(0.1f, Math.min(scaleFactor, 10.0f));
+            scaleFactor = Math.max(MIN_SCALE_FACTOR, Math.min(scaleFactor, MAX_SCALE_FACTOR));
 
+            float focusX = scaleGestureDetector.getFocusX();
+            float focusY = scaleGestureDetector.getFocusY();
+
+            if (MIN_SCALE_FACTOR < scaleFactor && scaleFactor < MAX_SCALE_FACTOR) {
+                scaleFromPoint(detector, focusX, focusY);
+            }
             invalidate();
             return true;
-
         }
 
-        @Override
-        public void onScaleEnd(ScaleGestureDetector detector) {
-            Log.d("Scale End", String.valueOf(scaleFactor));
+        private void scaleFromPoint(ScaleGestureDetector detector, float x, float y) {
+            translate(-x * (1-detector.getScaleFactor()),
+                    -y * (1-detector.getScaleFactor()));
         }
     }
 }
